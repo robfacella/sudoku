09d90bc179c113ee99aa8c55ffa1be9be22a07ea
Robert Facella <rob.facella@gmail.com>
2020-03-26 17:27:26 -0400

    testChoice

--- solver.py
+++ solver.py
@@ -1,5 +1,6 @@
 
 #vol266#page39#puzzle30
+global grid
 grid = [ [0,0,0,3,0,8,7,0,0],
          [1,0,0,5,0,0,0,3,2],
          [0,0,9,4,6,0,0,0,0],
@@ -14,5 +15,26 @@ def printGrid(theGrid):
     for line in theGrid:
         print(line)
 
+def possibleMove(x, y, n):
+    #Tests if choice of <n> is valid at location x,y in the grid
+    grid
+    #Test Columns and rows
+    for i in range(0,9) :
+        if grid[y][i] == n :
+            return False
+    for i in range(0,9) :
+        if grid[i][x] == n :
+            return False
+    #Test the 3x3 that the tile belongs to.
+    xSub = (x//3)*3
+    ySub = (y//3)*3
+    for i in range(0,3) :
+        for j in range(0,3) :
+            if grid[ySub+i][xSub+i] == n:
+                return False
+    #Otherwise the move is possible.
+    return True
 
-printGrid(grid)
+def solve(grid):
+    print(possibleMove(2,1,1))
+solve(grid)
