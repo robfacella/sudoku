57d286e177ac70fde13dc7b96a6c901a196a8ef4
Robert Facella <rob.facella@gmail.com>
2020-03-26 16:50:22 -0400

    printGrid

--- solver.py
+++ solver.py
@@ -1,4 +1,5 @@
 
+#vol266#page39#puzzle30
 grid = [ [0,0,0,3,0,8,7,0,0],
          [1,0,0,5,0,0,0,3,2],
          [0,0,9,4,6,0,0,0,0],
@@ -9,4 +10,9 @@ grid = [ [0,0,0,3,0,8,7,0,0],
          [8,9,0,0,0,6,0,0,3],
          [0,0,5,9,0,3,0,0,0]]
 
-         
+def printGrid(theGrid):
+    for line in theGrid:
+        print(line)
+
+
+printGrid(grid)
